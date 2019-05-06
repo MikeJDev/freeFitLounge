296c4043868f3c83ecc34ab46b29914cb253e07e
MikeJDev <ukmjanes@gmail.com>
2019-05-06 15:54:18 -0400

    added a component to render a button for retreiving workout data

--- client/app.js
+++ client/app.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Button from "react";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
   render() {
-    return <h1>Whats going on, manhghghg?</h1>;
+    return <h1>Welcome to FreeFitLounge Workout Generator</h1>;
+    <Button />
   }
 }
 export default App;
